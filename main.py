72c2fc3d745209b16f0ecf162529629d7850fcfc
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2011-12-16 17:43:46 +0100

    the treeview now shows files

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pygtk
 pygtk.require("2.0")
 import gtk
+import sys
+import os
 
 win = gtk.Window()
 
 entry = gtk.Entry()
 
-view = gtk.TreeView()
+files = gtk.ListStore(str,int)
+
+dircontent = os.listdir(os.getcwd())
+
+for f in dircontent:
+	files.append((f,os.stat(f).st_size))
+
+renderer = gtk.CellRendererText()
+
+view = gtk.TreeView(files)
+view.set_headers_visible(True)
+col1 = gtk.TreeViewColumn("Eimer",renderer,text=0)
+col2 = gtk.TreeViewColumn("Reimer",renderer,text=1)
+view.append_column(col1)
+view.append_column(col2)
 
 box = gtk.VBox()
 
@@ -16,6 +32,9 @@ box.add(view)
 win.add(box)
 
 win.show_all()
+
+	
+
 gtk.main()
 
 
