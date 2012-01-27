0bb7c14389e4b3def9e67d2b2cd241f4954661b5
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2012-01-27 23:45:12 +0100

    improved entry completion for tag dialog

--- tag.py
+++ tag.py
@@ -3,12 +3,13 @@ import tracker
 
 queries = {}
 
-queries["all tags and counts"] = """SELECT ?tag ?label COUNT(?im) WHERE {
+queries["all tags and counts"] = """SELECT ?tag ?label COUNT(?im) ?descr WHERE {
 	?im a nfo:Image;
 		nao:hasTag ?tag;
 		nie:isStoredAs ?file .
 	?tag a nao:Tag;
 		nao:prefLabel ?label .
+	OPTIONAL { ?tag nao:description ?descr }
 } GROUP BY ?label"""
 
 queries["tags for file"] = """SELECT ?label WHERE {
@@ -70,17 +71,23 @@ class ImageTagDialog(gtk.Dialog):
 		all_tags = [(str(x[1]), int(x[2])) for x in result]
 		all_tags = sorted(all_tags,cmp=lambda x,y: cmp(x[1],y[1]))
 
-		entry = gtk.Entry()
+		self.entry = gtk.Entry()
 		completion = gtk.EntryCompletion()
-		entry.set_completion(completion)
+		self.entry.set_completion(completion)
+		self.entry.connect("activate",self._entry_activate)
 		comp_store = gtk.ListStore(str)
 		completion.set_model(comp_store)
 		completion.set_text_column(0)
-		completion.set_inline_selection(True)
+		completion.set_inline_selection(False)
+		completion.set_inline_completion(False)
+		completion.set_match_func(self._match_func,None)
+		completion.connect("match-selected",self._match_selected)
+		completion.connect("cursor-on-match",lambda c,m,i: None)
+		completion.connect("insert-prefix",lambda c,p: None)
 		for tag,freq in all_tags:
 			comp_store.append([tag])
-		self.vbox.pack_start(entry)
-		entry.show()
+		self.vbox.pack_start(self.entry)
+		self.entry.show()
 
 		common_tags = []
 		for path in paths:
@@ -102,7 +109,9 @@ class ImageTagDialog(gtk.Dialog):
 		response = self.run()
 
 		if response == gtk.RESPONSE_ACCEPT:
-			new_tags = [x.strip() for x in entry.get_text().split(",")]
+			new_tags = []
+			if self.entry.get_text():
+				new_tags = [x.strip() for x in self.entry.get_text().split(",")]
 			for path in paths:
 				iter =  self.store.get_iter(path)
 				filename = self.store.get_value(iter,0)
@@ -113,7 +122,7 @@ class ImageTagDialog(gtk.Dialog):
 					tracker.update(queries["remove tag from file"] % (tag, filename))
 
 				for tag in new_tags:
-					if tag in file_tags:
+					if tag in file_tags or tag == "":
 						continue
 					if tag in all_tags:
 						tracker.update(queries["add existing tag"] % (filename,tag))
@@ -124,6 +133,26 @@ class ImageTagDialog(gtk.Dialog):
 				file_tags = [str(x[0]) for x in result]
 				self.store.set(iter, 3, ", ".join(file_tags))
 		self.destroy()
+
+	def _entry_activate(self,entry):
+		self.response(gtk.RESPONSE_ACCEPT)
+
+	def _match_func(self,completion, key, iter, func_data):
+		entry = key.split(",")[-1].strip().lower()
+		if len(entry) < 1:
+			return False
+		text = completion.get_model().get_value(iter,0).lower()
+		return text.startswith(entry)
+
+	def _match_selected(self,completion, model, iter):
+		text = model.get_value(iter,0)
+		entries = self.entry.get_text().split(",")
+		#this removes the fragment that we completed
+		entries[-1] = text
+		entries.append(" ")
+		self.entry.set_text(", ".join(entries))
+		self.entry.emit("move-cursor",gtk.MOVEMENT_BUFFER_ENDS,1,False)
+		return True
 
 	def _remove_tag(self,button,label):
 		self.removing.append(label)
@@ -143,14 +172,14 @@ class TagsDialog(gtk.Dialog):
 		self.removing = []
 
 		result = tracker.query(queries["all tags and counts"])
-		all_tags = [(str(x[1]), int(x[2])) for x in result]
+		all_tags = [(str(x[1]), int(x[2]), str(x[3])) for x in result]
 		all_tags = sorted(all_tags,cmp=lambda x,y: cmp(x[1],y[1]))
 
-		self.store = gtk.ListStore(str,int)
+		self.store = gtk.ListStore(str,int,str)
 		view = gtk.TreeView(self.store)
 		view.set_headers_visible(True)
 		renderer = gtk.CellRendererText()
-		column_titles = ["Tag", "Nr. of Files"]
+		column_titles = ["Tag", "Nr. of Files","Description"]
 		for title,idx in zip(column_titles,range(len(column_titles))):
 			col = gtk.TreeViewColumn(title,renderer,text=idx)
 			col.set_resizable(True)
@@ -161,8 +190,8 @@ class TagsDialog(gtk.Dialog):
 		scroll.add(view)
 		self.vbox.pack_start(scroll)
 
-		for tag, freq in all_tags:
-			self.store.append((tag, freq))
+		for tag, freq, descr in all_tags:
+			self.store.append((tag, freq, descr))
 
 		self.show_all()
 
